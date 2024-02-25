439807ea1f94da20dd9b3c935ea1fb19644ac286
Mark Munyaka <markmunyaka@gmail.com>
2024-02-25 15:30:55 +0000

    Add Naviagation: Done

--- my-website/src/routes/layout.tsx
+++ my-website/src/routes/layout.tsx
@@ -1,17 +1,17 @@
 import { component$, Slot } from "@builder.io/qwik";
-import type { RequestHandler } from "@builder.io/qwik-city";
-
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.builder.io/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
-};
+import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return <Slot />;
+  return (
+    <>
+        <nav>
+          <Link href="/">Home</Link>{' | '}
+          <Link href="/about">About</Link>{' | '}
+          <Link href="/conduct">Code of Conduct</Link>{' | '}
+          <Link href="/privacy">Privacy Policy</Link>{' | '}
+          <Link href="/blog">Blog</Link>
+        </nav>
+        <main><Slot /></main>
+    </>
+  );
 });
